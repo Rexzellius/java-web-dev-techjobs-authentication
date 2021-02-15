50701eab124ec908636c95289a3320e0fc247536
Rexzellius <katlynmmills@gmail.com>
2021-02-15 13:27:32 -0600

    editing

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
@@ -0,0 +1,39 @@
+package org.launchcode.javawebdevtechjobsauthentication;
+
+import org.launchcode.javawebdevtechjobsauthentication.controllers.AuthenticationController;
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        // the user is logged in
+        if (user != null) {
+            return true;
+        }
+
+        // the user is not logged in
+        response.sendRedirect("/login");
+        return false;
+    }
+
+}
